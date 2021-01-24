05e70f420a2a6cf2098848a143c67347a0047c3d
BlancaLujan <77300324+BlancaLujan@users.noreply.github.com>
2021-01-23 22:16:44 -0700

    Array

---
+++ array/array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5];
+
+            myArray[0] = 50;
+            myArray[1] = 100;
+            myArray[2] = 150;
+            myArray[3] = 200;
+            myArray[4] = 250;
+
+            foreach (var item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
